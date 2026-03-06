09c321b35bb7d6654eb35dc073323d326215c6a5
Unifieeed <unifieeed@gmail.com>
2026-03-05 21:33:53 -0300

    Create activator.ps1

---
+++ activator.ps1
@@ -0,0 +1,17 @@
+$hwid = (Get-CimInstance Win32_BaseBoard).SerialNumber.Trim()
+$keyword = Read-Host "Qual a sua chave do produto"
+
+$url = "https://zxqdfgfbfrbkqdasvkwm.supabase.co/functions/v1/hyper-endpoint"
+$body = @{ key = $keyword; hwid = $hwid } | ConvertTo-Json
+
+try {
+    $response = Invoke-RestMethod -Uri $url -Method Post -Body $body -ContentType "application/json"
+    
+    if ($response.status -eq "authorized") {
+        Write-Host "Acesso Permitido! Ativando..." -ForegroundColor Green
+        irm "https://luatools.vercel.app/install-plugin.ps1" | iex
+    }
+} catch {
+    # This catch block will capture the 403 Forbidden and the 401 Invalid
+    Write-Host "Acesso Negado: Chave invalida ou ja vinculada a outro computador." -ForegroundColor Red
+}
